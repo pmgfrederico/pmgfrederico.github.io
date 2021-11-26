11934c57e3407f67964a1c3f53b17e472d7fa2af
Pedro Frederico <pedro.frederico@mercedes-benz.io>
2021-11-26 15:25:39 +0000

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
-var par = document.createElement("p");
-var text = document.createTextNode("O conteúdo é introduzido pelo js bloqueado pelo robots. Crawl </=> de dependência.");
+const par = document.createElement("p");
+const text = document.createTextNode("O conteúdo é introduzido pelo js bloqueado pelo robots. Crawl </=> de dependência.");
 par.appendChild(text);
-document.body.appendChild(par);
+document.body.append(par);
