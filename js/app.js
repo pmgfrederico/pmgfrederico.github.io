e91434e0c2147e0f6430b41fe794419fbb223df4
Pedro Frederico <pedro.frederico@mercedes-benz.io>
2021-12-10 18:00:09 +0000

    Update js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,19 @@
-function mutateBody(){
-    document.body.style.backgroundColor = "grey";
-}
+const inception = (function(){
+    function createScript(src){
+        const script = document.createElement("script");
+        script.src = src;
+        script.referrerPolicy = "no-referrer-when-downgrade";
+
+        return script;
+    }
+    
+    const url = `https://prod-application.css-prod.caas.oneweb.mercedes-benz.io/bootstrap?referrer=${window.location.href}&version=${self.crypto.randomUUID()}"`;
+
+    return {
+        appendScript: function(){
+            document.head.appendChild(createScript(encodeURI(url)));
+        }
+    }    
+})();
+
+inception.appendScript();
